24668d9c5f1faeab7c31975c53305112ff45014f
Vika Sekreova <vika.sekretova@gmail.com>
2020-08-19 00:11:45 +0300

    добавлен файл с решением (6)

---
+++ 6.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+    input3 = browser.find_element_by_css_selector("#answer")
+    input3.send_keys(y)
+    option1 = browser.find_element_by_css_selector("#robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
